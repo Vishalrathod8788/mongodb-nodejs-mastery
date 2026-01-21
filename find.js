0b3aa8f9e47f6b44ae4db9f12d3f58ecfd047a05
Vishal Rathod <vishalrathod1562004@gmail.com>
2026-01-21 09:08:59 +0530

    Fix database connection handling and update import statement in find.js

--- find.js
+++ find.js
@@ -1,8 +1,30 @@
-import { connectToDatabase, client } from "./connection";
+import { connectToDatabase, client } from "./connection.js";
 
 const run = async () => {
-  const db = connectToDatabase();
-  if (!db) {
-    console.log("Database object not found!!");
+  try {
+    const db = await connectToDatabase();
+    if (!db) {
+      console.log("Database object not found!!");
+    }
+
+    const gradesCollection = db.collection("grades");
+    // const profileCollection = db.collection("profile");
+
+    const query = {
+      scores: {
+        $elemMatch: { type: "quiz", score: { $gte: 90 } },
+      },
+    };
+    // const option = { projection: { name: 1, age: 1, _id: 0 } };
+
+    const cursor = gradesCollection.find(query);
+    const result = await cursor.toArray();
+    console.log(result);
+  } catch (error) {
+    console.log("Error, Data not finding..");
+  } finally {
+    await client.close();
   }
 };
+
+run();
